d51737129516f4cd52b3ec7f4d74919cb6ccd12c
Vilovinto <vladvierboviy@gmail.com>
2024-09-06 16:22:45 +0300

    1 page done (index.html)

---
+++ index.js
@@ -0,0 +1,24 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(users => {
+        let divBody = document.createElement('div');
+        divBody.classList.add('users');
+        document.body.appendChild(divBody);
+        for (const user of users) {
+            let divUsers = (document.createElement('div'));
+            divUsers.classList.add('user');
+            let id = document.createElement('p');
+            id.classList.add('p');
+            id.innerText = `ID: ${user.id}`;
+            let name = document.createElement('p');
+            name.classList.add('p');
+            name.innerText = `Name: ${user.name}`;
+            let button = document.createElement('button');
+            button.innerText = 'Details';
+            button.addEventListener('click', () => {
+                location.href = 'user-details.html?id=' + user.id;
+            })
+            divUsers.append(id, name, button);
+            divBody.appendChild(divUsers);
+        }
+    })
